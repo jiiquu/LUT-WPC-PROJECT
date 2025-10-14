6e409976159bfbad8d7a275d8cc7f43fd0e403f8
jiiquu <74710058+jiiquu@users.noreply.github.com>
2025-10-14 21:50:03 +0300

    Added changing location and showing current weather info with icon and description

--- JS/weather.js
+++ JS/weather.js
@@ -8,62 +8,67 @@ if ("geolocation" in navigator) {
       myLat = position.coords.latitude;
       console.log("Longitude:", position.coords.longitude);
       myLon = position.coords.longitude;
-      fetchOMCurrentWeather();
+      const OWMCurrentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLon}&appid=58111013c30dffa44e8a1d78e22ce00c&units=metric`;
+      fetchCurrentWeather(OWMCurrentWeatherUrl);
 
     },
     (error) => {
       console.error("Error getting location:", error);
-      fetchOMCurrentWeather();
+      const OWMCurrentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLon}&appid=58111013c30dffa44e8a1d78e22ce00c&units=metric`;
+      fetchCurrentWeather(OWMCurrentWeatherUrl);
 
     }
   );
 } else {
-  console.log("Geolocation is not supported by this browser.");
-  fetchOMCurrentWeather();
+    console.log("Geolocation is not supported by this browser.");
+    const OWMCurrentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLon}&appid=58111013c30dffa44e8a1d78e22ce00c&units=metric`;
+    fetchCurrentWeather(OWMCurrentWeatherUrl);
 
 }
 
 const MIUrl = 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=60.10&lon=9.58';
-const OWMUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Helsinki&appid=58111013c30dffa44e8a1d78e22ce00c&units=metric';
 
 
+//const OMCurrentWeatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${myLat}&longitude=${myLon}&current=temperature_2m,is_day,rain,weather_code,wind_speed_10m&wind_speed_unit=ms`;
+//const OWMCurrentWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Helsinki&appid=58111013c30dffa44e8a1d78e22ce00c&units=metric';
 
-async function fetchOMCurrentWeather() {
-    const OMCurrentWeatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${myLat}&longitude=${myLon}&current=temperature_2m,is_day,rain,weather_code,wind_speed_10m&wind_speed_unit=ms`;
-    const response = await fetch(OMCurrentWeatherUrl);
-    //const response = await fetch(MIUrl);
+
+async function fetchCurrentWeather(url) {
+
+    const response = await fetch(url);
     const data = await response.json();
     console.log(data);
     displayCurrentWeather(data);
-    //document.getElementById('weather').innerText = `Current temperature: ${data.current.temperature_2m}°C, Wind Speed: ${data.current.wind_speed_10m}`;
-}    
-async function fetchOWMCurrentWeather() {
-    const response = await fetch(OWMUrl);
-    const data = await response.json();
-    console.log(data);
-    //displayCurrentWeather(data);
-
-};
-function displayCurrentWeather(data) {
-    //const div = document.getElementById('current');
-    document.querySelector('#current_temp').innerText = data.current.temperature_2m;
-    document.querySelector('#current_rainfall').innerText = data.current.rain;
-    document.querySelector('#current_wind_speed').innerText = data.current.wind_speed_10m;
-
     
+    function displayCurrentWeather(data) {
+    //const div = document.getElementById('current');
+        if (!data || data.length === 0) {
+            console.error('No data found');
+            return;
+        }
+        document.querySelector('#city').innerText = data.name + ', ' + data.sys.country;
+        document.querySelector('#current_time').innerText = new Date((data.dt + data.timezone) * 1000).toUTCString().replace('GMT', '');
+        document.querySelector('#current_icon').src=`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        document.querySelector('#current_condition').innerText = data.weather[0].description;
+        document.querySelector('#current_temp').innerText = data.main.temp;
+        document.querySelector('#current_humidity').innerText = data.main.humidity;
+        document.querySelector('#current_wind_speed').innerText = data.wind.speed;
+
+    };
 
 }
+
+document.getElementById('search_form').addEventListener('submit', async(event) => {
+    event.preventDefault();
+    const city = document.getElementById('city_input').value;
+    if (city !== '') {
+        const OWMCurrentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=58111013c30dffa44e8a1d78e22ce00c&units=metric`;
+        fetchCurrentWeather(OWMCurrentWeatherUrl);
+    }
+    else {
+        return;
+    }
     
-    //const table = document.createElement('table');
-   /*  for (const [key, value] of Object.entries(data.current)) {
-        //console.log(key, value);
-        const row = document.createElement('tr');
-        const keyCell = document.createElement('td');
-        keyCell.innerText = key;
-        const valueCell = document.createElement('td');
-        valueCell.innerText = value;
-        row.appendChild(keyCell);
-        row.appendChild(valueCell);
-        table.appendChild(row);
-        div.appendChild(table); */
-    //}
+
+})
+
